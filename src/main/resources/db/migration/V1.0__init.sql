a391cd566a6b6fd9cd30cc4705e8f29979006c81
Chen Xiaowei <313455671@qq.com>
2025-04-15 11:25:03 +0800

    fix: init sql

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -81,26 +81,6 @@ CREATE TABLE contractor.contractor_classification (
 ALTER TABLE contractor.contractor_classification OWNER TO postgres;
 
 --
--- Name: flyway_contractor_history; Type: TABLE; Schema: contractor; Owner: postgres
---
-
-CREATE TABLE contractor.flyway_contractor_history (
-                                                      installed_rank integer NOT NULL,
-                                                      version character varying(50),
-                                                      description character varying(200) NOT NULL,
-                                                      type character varying(20) NOT NULL,
-                                                      script character varying(1000) NOT NULL,
-                                                      checksum integer,
-                                                      installed_by character varying(100) NOT NULL,
-                                                      installed_on timestamp without time zone DEFAULT now() NOT NULL,
-                                                      execution_time integer NOT NULL,
-                                                      success boolean NOT NULL
-);
-
-
-ALTER TABLE contractor.flyway_contractor_history OWNER TO postgres;
-
---
 -- Data for Name: classification; Type: TABLE DATA; Schema: contractor; Owner: postgres
 --
 
